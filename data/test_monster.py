08c43f20d07af3b5f9163523fa04da407672eba3
permadihendra <hendrapermadi69@gmail.com>
2025-11-27 14:52:45 +0700

    add test if its string

--- data/test_monster.py
+++ data/test_monster.py
@@ -4,3 +4,8 @@ from monster import get_monster
 def test_get_monster():
     data = get_monster()
     assert isinstance(data, dict)
+
+
+def test_is_monster_int():
+    data = get_monster()
+    assert isinstance(data["name"], str)
