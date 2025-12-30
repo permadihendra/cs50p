cdb591fded5b2b62a6d6bddb1328c9d26b045d3c
permadihendra <hendrapermadi69@gmail.com>
2025-12-30 21:49:40 +0700

    create character with class

--- character.py
+++ character.py
@@ -1,11 +1,94 @@
-class Character:
-    def __init__(self, name: str, job: str):
-        self.name: str = name
-        self.job: str = job
+from typing import override
 
+
+class Stat:
+    def __init__(self, health: int, attack: int, defend: int, agility: int):
+        self.health: int = health
+        self.attack: int = attack
+        self.defend: int = defend
+        self.agility: int = agility
+
+class Item(Stat):
+    def __init__(self, id:int, name: str, type: str, 
+                 health: int=0, attack: int=0, defend: int=0, agility: int=0):
+        super().__init__(health, attack, defend, agility)
+        self.id: int = id
+        self.name: str = name
+        self.type: str = type
+
+class Character(Stat):
+    # class atribute
+    health: int
+    attack: int
+    defend: int
+    agility: int
+    inventory: list[Item] = [ ]
+    equipment: dict[str, bool] = {"sword": False, "shield": False}
+
+    def __init__(
+        self, id: int, name: str, health: int, attack: int, defend: int, agility: int
+    ):
+        super().__init__(health, attack, defend, agility)
+        self.id: int = id
+        self.name: str = name
+
+    def equip_item(self, item:Item)-> None:
+        if self.equipment[item.type] is True:
+            print(f"Item '{item.type}' already equiped")
+            return
+        elif item not in self.inventory:
+            print(f"Item {item.name} not found in inventory")
+            return
+        else:
+            self.health += item.health
+            self.attack += item.attack
+            self.defend += item.defend
+            self.agility += item.agility
+            self.inventory.remove(item)
+            self.equipment[item.type] = True
+            print(f"Successfull equip: {item.name} !") 
+           
+
+    def save_item(self, item: Item):
+        self.inventory.append(item)
+   
+    @override
+    def __str__(self) -> str:
+        return (
+f"""
+=========================
+CHARACTER INFO
+=========================
+ID   : {self.id}
+NAME : {self.name}
+-------------------------
+STATS
+-------------------------
+Health  : {self.health}
+Attack  : {self.attack}
+Defend  : {self.defend}
+Agility : {self.agility}
+-------------------------
+INVENTORY
+-------------------------
+{[item.name for item in self.inventory]}
+
+        """)
 
 def main():
-    print("Character")
+    char = Character(1, "Joni", 100, 35, 30, 20)
+    sword = Item(id=1, name="Long Sword", type="sword", attack=10)
+    shield = Item(id=1, name="Iron Shield", type="shield", defend=15)
+    
+    char.save_item(sword)
+    #char.save_item(shield)
+
+    char.equip_item(sword) 
+    char.equip_item(shield)
+
+    print(char)
+
+    char.equip_item(shield)
 
 
 if __name__ == "__main__":
